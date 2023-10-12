b2a558e27be9bdba665773ec4b3b6ce8a467195b
LlayN <95718887+LlayN@users.noreply.github.com>
2023-10-12 18:39:01 +0200

    MAJ Loader

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const btnRegister = document.querySelector('.btn-register')
 const divnewGames = document.querySelector('.divnewGames')
 
 
+
 /* CODE POUR BOUTON LATERAL MENU */
 
 let checkLateral = window.innerWidth <= 767;
@@ -15,6 +16,7 @@ buttonLateralPc.addEventListener('click', () => {
   showDivMobile.style.display = 'block'
   if(window.innerWidth >= 767){
     divnewGames.style.paddingLeft = '60px'
+    divGameLaunch.style.paddingLeft = '60px'
   }
   
 })
@@ -24,6 +26,7 @@ buttonLateralMobile.addEventListener('click', () => {
   showDivPc.style.display = 'block'
   if(window.innerWidth >= 767){
     divnewGames.style.paddingLeft = '210px'
+
   }
   
 })
@@ -35,12 +38,14 @@ function handleResize() {
     showDivMobile.style.display = 'block';
     divnewGames.style.paddingLeft = '60px'
 
+
   } 
   else if (window.innerWidth > 767 && checkLateral) {
     checkLateral = false;
     showDivMobile.style.display = 'none';
     showDivPc.style.display = 'block';
     divnewGames.style.paddingLeft = '210px'
+
   }
 }
 
@@ -49,6 +54,7 @@ function loadRezise(){
     showDivPc.style.display = 'none'
     showDivMobile.style.display = 'block'
     divnewGames.style.paddingLeft = '60px'
+
   }
 }
 
